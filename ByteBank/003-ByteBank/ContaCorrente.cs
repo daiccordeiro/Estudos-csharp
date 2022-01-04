036c9fd942d5f8f25c88ce1dc74291be7b31037d
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-01-04 15:01:25 -0300

    Referências

---
+++ ByteBank/003-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿
+public class ContaCorrente 
+// classe contacorrente define a estrutura que os objetvos dessa classe devem seguir!
+
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo=100;
+}
