c8662ab5af7e13623a3856ecf0700f44b00aa97e
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-01-17 11:01:29 -0300

    Método e função

---
+++ ByteBank/004-ByteBank/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿
+public class ContaCorrente 
+// classe contacorrente define a estrutura que os objetvos dessa classe devem seguir!
+
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo=100;
+
+    //Quando retorna valor é comum chamar de FUNÇÃO.
+    //Função para realização de SAQUES das contas correntes. (Quando usa o 'bool' precisa ter um retorno, true ou false.
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+                
+        this.saldo -= valor;
+        return true;        
+    }
+
+    //Quando NÃO retorna valor é comum chamar de MÉTODO.
+    //Função para DEPOSITAR. (Para ações sem retorno, usa-se o 'void')
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    //Função para TRANSFERIR, onde retorna valor e precisa do parâmetro 'valor' e 'conta'
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        //Manter primeiro as condições que retornam false, 
+        //assim não há necessidade de usar o else no bool quando for true.
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+    }
+
+}
---
+++ ByteBank/004-ByteBank/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nova contacorrente 'Bruno'
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+            
+            Console.WriteLine(contaDoBruno.saldo);
+
+            //contaDoBruno = (nome da referência) e .Sacar = (nome do método)
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            //Verificando o saldo da conta 'Bruno' depois da função 'SACAR'
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+            
+            //Método DEPOSITAR na contaDoBruno
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            //Nova contacorrente 'Gabriela'
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+
+            //Verificando os saldos das contas 'Bruno' e 'Gabriela' depois do método 'DEPOSITAR'
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            //Chamando a função TRANSFERIR - contaDoBruno para contaDaGabriela
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            //Verificando os saldos das contas 'Bruno' e 'Gabriela' depois da função 'TRANSFERIR'
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            //Retorno booleano da transferencia (true ou false)
+            Console.WriteLine("Resultado transferência: " + resultadoTransferencia);
+            
+            //TRANSFERIR da contaDaGabriela para contaDoBruno
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            //Mostra na tela
+            Console.ReadLine();
+
+        }
+    }
+}
