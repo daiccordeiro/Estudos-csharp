afa213f8ecbe825dac9d1fd204b0a9906c65267e
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-01-19 14:18:41 -0300

    Construtores e propriedades estáticas

---
+++ ByteBank/007-ByteBank/ContaCorrente.cs
@@ -0,0 +1,117 @@
+﻿
+namespace _007_ByteBank
+{
+    public class ContaCorrente
+    // classe contacorrente define a estrutura que os objetvos dessa classe devem seguir!
+
+    {
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+
+        public int Numero { get; set; }
+
+        //PRIVATE deixa 'saldo' como campo interno da Classe 'ContaCorrente'
+        private double _saldo = 100;
+
+        //Construção do Método com Get e Set
+        public double Saldo
+        {
+            //GET substituiu ObterSaldo
+            get //get precisa ter um retorno
+            {
+                return _saldo;
+            }
+            //SET substituiu DefinirSaldo
+            set //set é possível fazer alteração
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        //<--- INÍCIO --->
+        //Foi substituido por GET e SET acima
+        ////Método para saber se  a operação realizada com sucesso ou não.
+        //public void DefinirSaldo(double saldo)
+        //{
+        //    if(saldo < 0)
+        //    {
+        //        return;
+        //    }
+        //    this.saldo = saldo;
+        //}
+
+        ////Método que dá acesso ao valor do campo 'saldo'
+        //public double ObterSaldo()
+        //{
+        //    return saldo;
+        //}
+        //<--- FIM --->
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        //Quando retorna valor é comum chamar de FUNÇÃO.
+        //Função para realização de SAQUES das contas correntes. (Quando usa o 'bool' precisa ter um retorno, true ou false.
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        //Quando NÃO retorna valor é comum chamar de MÉTODO.
+        //Função para DEPOSITAR. (Para ações sem retorno, usa-se o 'void')
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        //Função para TRANSFERIR, onde retorna valor e precisa do parâmetro 'valor' e 'conta'
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            //Manter primeiro as condições que retornam false, 
+            //assim não há necessidade de usar o else no bool quando for true.
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+}
