849a893c42aea0340a359186c9790b197ffa4c0e
DAIANE CRISTINA CORDEIRO <67031248+daiccordeiro@users.noreply.github.com>
2022-03-18 11:52:37 -0300

    Finalização Projeto Asp.net MVC 5

--- MVC/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ MVC/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -31,6 +31,7 @@ namespace CaelumEstoque.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Adiciona(Produto produto)
         {
             int idDaInformatica = 1;
